f4806b334a2f10ef0b33b9701d2527f739afc11a
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-02-14 18:23:14 +0300

    [HACK] Move compile time lowering up in js lowering phases
    This change was caused by several facts:
    - IrInterpreter need information that isn't available in old phase
    position
    - Now it will be easier to move in jvm phases

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsLoweringPhases.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsLoweringPhases.kt
@@ -735,8 +735,7 @@ val compileTimeEvaluationPhase = makeJsModulePhase(
     ::CompileTimeCalculationLowering,
     name = "CompileTimeEvaluation",
     //TODO change annotation to modifier
-    description = "Evaluate calls that are marked with @CompileTimeCalculation annotation"
-    //TODO find best position
+    description = "Evaluate calls that are marked with @CompileTimeCalculation annotation",
 ).toModuleLowering()
 
 private val cleanupLoweringPhase = makeBodyLoweringPhase(
@@ -755,6 +754,7 @@ private val loweringList = listOf<Lowering>(
     scriptRemoveReceiverLowering,
     validateIrBeforeLowering,
     expectDeclarationsRemovingPhase,
+    compileTimeEvaluationPhase,
     stripTypeAliasDeclarationsPhase,
     jsCodeOutliningPhase,
     arrayConstructorReferencePhase,
@@ -787,7 +787,6 @@ private val loweringList = listOf<Lowering>(
     delegateToPrimaryConstructorLoweringPhase,
     annotationConstructorLowering,
     initializersLoweringPhase,
-    compileTimeEvaluationPhase,
     initializersCleanupLoweringPhase,
     kotlinNothingValueExceptionPhase,
     // Common prefix ends
