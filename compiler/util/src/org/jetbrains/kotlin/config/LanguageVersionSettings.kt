20c11d09d4179a61173a46d4d2eec6c11c14e3f5
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-03-01 20:02:23 +0300

    Add new language feature CompileTimeCalculations

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -237,6 +237,7 @@ enum class LanguageFeature(
         State.ENABLED_WITH_WARNING
     ),
     MultiPlatformProjects(sinceVersion = null, defaultState = State.DISABLED),
+    CompileTimeCalculations(sinceVersion = null, defaultState = State.DISABLED),
     InlineClasses(KOTLIN_1_3, defaultState = State.ENABLED_WITH_WARNING, kind = UNSTABLE_FEATURE),
     ProhibitComparisonOfIncompatibleClasses(sinceVersion = null, kind = BUG_FIX, defaultState = State.DISABLED),
 
