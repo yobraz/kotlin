bc3c328d0908a935e772e9f3b17b5d140c402a91
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-05-29 01:04:54 +0300

    [HACK] Add compile time evaluation phase to jvm lowering

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/JvmLower.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/JvmLower.kt
@@ -313,6 +313,14 @@ private fun codegenPhase(generateMultifileFacade: Boolean): NamedCompilerPhase<J
     )
 }
 
+val compileTimeEvaluationPhase = makeIrModulePhase(
+    ::CompileTimeCalculationLowering,
+    name = "CompileTimeEvaluation",
+    //TODO change annotation to modifier
+    description = "Evaluate calls that are marked with @CompileTimeCalculation annotation",
+    prerequisite = setOf(expectDeclarationsRemovingPhase)
+)
+
 private val jvmFilePhases = listOf(
     typeAliasAnnotationMethodsPhase,
     stripTypeAliasDeclarationsPhase,
@@ -430,6 +438,7 @@ private val jvmLoweringPhases = NamedCompilerPhase(
     lower = validateIrBeforeLowering then
             processOptionalAnnotationsPhase then
             expectDeclarationsRemovingPhase then
+            compileTimeEvaluationPhase then
             serializeIrPhase then
             scriptsToClassesPhase then
             fileClassPhase then
