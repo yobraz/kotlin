c1523366adda054a98432901ad582d81ede42bc0
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-06-10 23:46:23 +0300

    Add test to check interpretation of jvm static fields

--- compiler/tests-for-compiler-generator/tests/org/jetbrains/kotlin/test/generators/GenerateJUnit3CompilerTests.kt
+++ compiler/tests-for-compiler-generator/tests/org/jetbrains/kotlin/test/generators/GenerateJUnit3CompilerTests.kt
@@ -139,7 +139,7 @@ fun generateJUnit3CompilerTests(args: Array<String>) {
             }
 
             testClass<AbstractJsInterpreterTestCase> {
-                model("ir/interpreter", pattern = "^(.+)\\.kt(s)?\$")
+                model("ir/interpreter", pattern = "^(.+)\\.kt(s)?\$", excludeDirs = listOf("jvm"))
             }
 
             testClass<AbstractJvmInterpreterTestCase> {
