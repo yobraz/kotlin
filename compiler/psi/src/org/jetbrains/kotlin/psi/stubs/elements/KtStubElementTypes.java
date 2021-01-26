54ec5e280c666c50201ffe694459403b2a3fb540
Anastasiya Shadrina <a.shadrina5@mail.ru>
2021-01-26 19:55:46 +0700

    [Parser] Introduce "context" soft keyword and parse context receivers

--- compiler/psi/src/org/jetbrains/kotlin/psi/stubs/elements/KtStubElementTypes.java
+++ compiler/psi/src/org/jetbrains/kotlin/psi/stubs/elements/KtStubElementTypes.java
@@ -142,6 +142,9 @@ public interface KtStubElementTypes {
     KtPlaceHolderStubElementType<KtConstructorCalleeExpression> CONSTRUCTOR_CALLEE =
             new KtPlaceHolderStubElementType<>("CONSTRUCTOR_CALLEE", KtConstructorCalleeExpression.class);
 
+    KtPlaceHolderStubElementType<KtContextReceiver> CONTEXT_RECEIVER =
+            new KtPlaceHolderStubElementType<>("CONTEXT_RECEIVER", KtContextReceiver.class);
+
     KtConstantExpressionElementType NULL                = new KtConstantExpressionElementType("NULL");
     KtConstantExpressionElementType BOOLEAN_CONSTANT    = new KtConstantExpressionElementType("BOOLEAN_CONSTANT");
     KtConstantExpressionElementType FLOAT_CONSTANT      = new KtConstantExpressionElementType("FLOAT_CONSTANT");
