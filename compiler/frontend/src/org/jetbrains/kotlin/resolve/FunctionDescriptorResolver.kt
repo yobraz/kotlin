eba2112c8fb878e2cd5e1c663d1dbaf082d7c01c
Anastasiya Shadrina <a.shadrina5@mail.ru>
2021-01-26 17:41:31 +0700

    [FE] Add context receivers getter to KtCallableDeclaration interface

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/FunctionDescriptorResolver.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/FunctionDescriptorResolver.kt
@@ -196,6 +196,9 @@ class FunctionDescriptorResolver(
                 if (function is KtFunctionLiteral) expectedFunctionType.getReceiverType() else null
             }
 
+        function.contextReceiverTypeReferences.onEach {
+            typeResolver.resolveType(headerScope, it, trace, true)
+        }
 
         val valueParameterDescriptors =
             createValueParameterDescriptors(function, functionDescriptor, headerScope, trace, expectedFunctionType, inferenceSession)
