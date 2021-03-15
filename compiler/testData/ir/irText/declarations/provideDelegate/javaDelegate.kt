637401f8a61426707acf6a89426498fea72e0bc4
Roman Artemev <roman.artemev@jetbrains.com>
2021-03-15 15:34:33 +0300

    [IR] Add irText test for KT-45297

---
+++ compiler/testData/ir/irText/declarations/provideDelegate/javaDelegate.kt
@@ -0,0 +1,30 @@
+// WITH_RUNTIME
+
+// FILE: box.kt
+
+package k
+
+import J
+
+var p1 by J()::foo
+var p2 by J()::foo
+
+fun box(): String {
+    p1 = "O"
+    p2 = "K"
+    return p1 + p2
+}
+
+// FILE: J.java
+
+public class J {
+    private String s = "Fail";
+
+    public void setFoo(String s) {
+        this.s = s;
+    }
+
+    public String getFoo() {
+        return s;
+    }
+}
