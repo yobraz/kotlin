348df6dad21bdadd56b78c0a7d5c82b622f2b0f7
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2021-03-16 01:23:28 +0100

    Check for backend when checking for suspend in fun interfaces

---
+++ compiler/frontend.java/src/org/jetbrains/kotlin/resolve/jvm/checkers/SuspendInFunInterfaceChecker.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.resolve.jvm.checkers
+
+import org.jetbrains.kotlin.config.JvmAnalysisFlags
+import org.jetbrains.kotlin.config.LanguageFeature
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.resolve.checkers.DeclarationChecker
+import org.jetbrains.kotlin.resolve.checkers.DeclarationCheckerContext
+import org.jetbrains.kotlin.resolve.sam.getAbstractMembers
+import org.jetbrains.kotlin.resolve.source.getPsi
+
+class SuspendInFunInterfaceChecker : DeclarationChecker {
+    override fun check(declaration: KtDeclaration, descriptor: DeclarationDescriptor, context: DeclarationCheckerContext) {
+        if (declaration !is KtClass) return
+        if (descriptor !is ClassDescriptor || !descriptor.isFun) return
+
+        val funKeyword = declaration.getFunKeyword() ?: return
+
+        val abstractMembers = getAbstractMembers(descriptor)
+        val abstractMember = abstractMembers.filterIsInstance<FunctionDescriptor>().singleOrNull() ?: return
+        if (!abstractMember.isSuspend) return
+
+        if (context.languageVersionSettings.supportsFeature(LanguageFeature.SuspendFunctionsInFunInterfaces) &&
+            context.languageVersionSettings.getFlag(JvmAnalysisFlags.useIR)
+        ) return
+
+        val ktFunction = abstractMember.source.getPsi() as? KtNamedFunction
+        val reportOn = ktFunction?.modifierList?.getModifier(KtTokens.SUSPEND_KEYWORD) ?: funKeyword
+        context.trace.report(Errors.FUN_INTERFACE_WITH_SUSPEND_FUNCTION.on(reportOn))
+    }
+}
