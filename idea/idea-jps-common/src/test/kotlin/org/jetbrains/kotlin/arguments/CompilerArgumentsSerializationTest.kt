806431370e6391511116e979daf4e050ce94cb59
Yaroslav Chernyshev <yaroslav.chernyshev@jetbrains.com>
2021-03-18 15:48:26 +0300

    Add more complicated tests with generating random values

--- idea/idea-jps-common/src/test/kotlin/org/jetbrains/kotlin/arguments/CompilerArgumentsSerializationTest.kt
+++ idea/idea-jps-common/src/test/kotlin/org/jetbrains/kotlin/arguments/CompilerArgumentsSerializationTest.kt
@@ -5,9 +5,13 @@
 
 package org.jetbrains.kotlin.arguments
 
+import org.jdom.Element
 import org.jetbrains.kotlin.cli.common.arguments.*
+import org.jetbrains.kotlin.utils.addToStdlib.cast
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 import org.junit.Test
+import kotlin.random.Random
+import kotlin.reflect.KMutableProperty1
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.memberProperties
 
@@ -19,8 +23,38 @@ class CompilerArgumentsSerializationTest {
     }
 
     @Test
+    fun testRandomFlagArgumentsJVM() {
+        doRandomFlagArgumentsTest<K2JVMCompilerArguments>()
+    }
+
+    @Test
+    fun testRandomStringArgumentsJVM() {
+        doRandomStringArgumentsTest<K2JVMCompilerArguments>()
+    }
+
+    @Test
+    fun testRandomArrayArgumentsJVM() {
+        doRandomArrayArgumentsTest<K2JVMCompilerArguments>()
+    }
+
+    @Test
     fun testDummyJs() {
         doSerializeDeserializeAndCompareTest<K2JSCompilerArguments>()
+    }
+
+    @Test
+    fun testRandomFlagArgumentsJS() {
+        doRandomFlagArgumentsTest<K2JSCompilerArguments>()
+    }
+
+    @Test
+    fun testRandomStringArgumentsJS() {
+        doRandomStringArgumentsTest<K2JSCompilerArguments>()
+    }
+
+    @Test
+    fun testRandomArrayArgumentsJS() {
+        doRandomArrayArgumentsTest<K2JSCompilerArguments>()
     }
 
     @Test
@@ -29,21 +63,96 @@ class CompilerArgumentsSerializationTest {
     }
 
     @Test
+    fun testRandomFlagArgumentsMetadata() {
+        doRandomFlagArgumentsTest<K2MetadataCompilerArguments>()
+    }
+
+    @Test
+    fun testRandomStringArgumentsMetadata() {
+        doRandomStringArgumentsTest<K2MetadataCompilerArguments>()
+    }
+
+    @Test
+    fun testRandomArrayArgumentsMetadata() {
+        doRandomArrayArgumentsTest<K2MetadataCompilerArguments>()
+    }
+
+
+    @Test
     fun testDummyJsDce() {
         doSerializeDeserializeAndCompareTest<K2JSDceArguments>()
     }
 
+    @Test
+    fun testRandomFlagArgumentsJSDce() {
+        doRandomFlagArgumentsTest<K2JSDceArguments>()
+    }
+
+    @Test
+    fun testRandomStringArgumentsJSDce() {
+        doRandomStringArgumentsTest<K2JSDceArguments>()
+    }
+
+    @Test
+    fun testRandomArrayArgumentsJSDce() {
+        doRandomArrayArgumentsTest<K2JSDceArguments>()
+    }
+
+
     private inline fun <reified T : CommonToolArguments> doSerializeDeserializeAndCompareTest(configure: T.() -> Unit = {}) {
         val oldInstance = T::class.java.newInstance().apply(configure)
         val serializer = CompilerArgumentsSerializerV4<T>()
+        val mockFacetElement = Element("ROOT")
         val element = serializer.serialize(oldInstance)
+        mockFacetElement.addContent(element)
         val newInstance = T::class.java.newInstance()
         val deserializer = CompilerArgumentsDeserializerV4(newInstance)
-        val deserializedArguments = deserializer.deserialize(element)
+        val deserializedArguments = deserializer.deserialize(mockFacetElement)
         T::class.memberProperties.mapNotNull { it.safeAs<KProperty1<T, *>>() }.forEach {
             assert(it.get(oldInstance) == it.get(deserializedArguments)) {
                 "Property ${it.name} has different values before (${it.get(oldInstance)}) and after (${it.get(deserializedArguments)}) serialization"
             }
         }
+    }
+
+    private inline fun <reified T : CommonToolArguments> doRandomFlagArgumentsTest() {
+        val flagProperties = CompilerArgumentsContentProspector.getFlagCompilerArgumentProperties(T::class)
+        val randomFlags = generateSequence { Random.nextBoolean() }.take(flagProperties.size).toList()
+        doSerializeDeserializeAndCompareTest<T> {
+            flagProperties.zip(randomFlags).forEach {
+                it.first.cast<KMutableProperty1<T, Boolean>>().set(this, it.second)
+            }
+        }
+    }
+
+    private inline fun <reified T : CommonToolArguments> doRandomStringArgumentsTest() {
+        val stringProperties = CompilerArgumentsContentProspector.getStringCompilerArgumentProperties(T::class)
+        val randomStrings = generateSequence { generateRandomString(10) }.take(stringProperties.size).toList()
+        doSerializeDeserializeAndCompareTest<T> {
+            stringProperties.zip(randomStrings).forEach {
+                it.first.cast<KMutableProperty1<T, String?>>().set(this, it.second)
+            }
+        }
+    }
+
+    private inline fun <reified T : CommonToolArguments> doRandomArrayArgumentsTest() {
+        val arrayProperties = CompilerArgumentsContentProspector.getArrayCompilerArgumentProperties(T::class)
+        val randomArrays = generateSequence {
+            generateSequence { generateRandomString(10) }.take(2).toList().toTypedArray()
+        }.take(arrayProperties.size).toList()
+        doSerializeDeserializeAndCompareTest<T> {
+            arrayProperties.zip(randomArrays).forEach {
+                it.first.cast<KMutableProperty1<T, Array<String>?>>().set(this, it.second)
+            }
+        }
+    }
+
+    companion object {
+        private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+        private fun generateRandomString(length: Int) = (1..length)
+            .map { i -> Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("")
     }
 }
