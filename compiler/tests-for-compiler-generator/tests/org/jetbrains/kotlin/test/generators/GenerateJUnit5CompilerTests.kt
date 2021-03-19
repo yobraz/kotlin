0d9064a298b6656fa758812659e6bbeb5784834e
Georgy Bronnikov <georgy.bronnikov@jetbrains.com>
2021-03-19 14:59:03 +0300

    JVM_IR: run Kotlin against Kotlin tests with serialization

--- compiler/tests-for-compiler-generator/tests/org/jetbrains/kotlin/test/generators/GenerateJUnit5CompilerTests.kt
+++ compiler/tests-for-compiler-generator/tests/org/jetbrains/kotlin/test/generators/GenerateJUnit5CompilerTests.kt
@@ -109,6 +109,10 @@ fun generateJUnit5CompilerTests(args: Array<String>) {
                 model("codegen/boxInline")
             }
 
+            testClass<AbstractIrSerializeCompileKotlinAgainstInlineKotlinTest> {
+                model("codegen/boxInline")
+            }
+
             testClass<AbstractJvmIrAgainstOldBoxInlineTest> {
                 model("codegen/boxInline")
             }
