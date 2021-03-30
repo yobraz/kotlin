4b6cc430b844a96d1913bd4d7d811302808948e6
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2021-03-30 17:46:21 +0300

    JVM_IR KT-45697 reference classes from sources before IR generation

---
+++ compiler/testData/codegen/box/sameFileInSourceAndDependencies/nestedClassDeclaration.kt
@@ -0,0 +1,29 @@
+// MODULE: lib
+// FILE: 2.kt
+class Host {
+    abstract class B : A()
+
+    abstract class A {
+        private val value = "OK"
+        fun f() = value
+    }
+}
+
+// FILE: 3.kt
+abstract class C : Host.B()
+
+// MODULE: main(lib)
+// FILE: 1.kt
+class D : C()
+
+fun box(): String = D().f()
+
+// FILE: 2.kt
+class Host {
+    abstract class B : A()
+
+    abstract class A {
+        private val value = "OK"
+        fun f() = value
+    }
+}
---
+++ compiler/testData/codegen/box/sameFileInSourceAndDependencies/propertyDeclaration.kt
@@ -0,0 +1,17 @@
+// MODULE: lib
+// FILE: 2.kt
+val a get() = "OK"
+val b get() = a
+
+// FILE: 3.kt
+val c get() = b
+
+// MODULE: main(lib)
+// FILE: 1.kt
+val d get() = c
+
+fun box(): String = d
+
+// FILE: 2.kt
+val a get() = "OK"
+val b get() = a
