b146a5805690c9b49e133b420b0669a3867c573a
Nikolay Krasko <goodwinnk@gmail.com>
2021-04-26 15:41:04 +0300

    Drop bintray credentials

--- gradle/kotlinPluginPublication.gradle.kts
+++ gradle/kotlinPluginPublication.gradle.kts
@@ -37,16 +37,6 @@ configure<PublishingExtension> {
     repositories {
         maven {
             url = uri(findProperty("deployRepoUrl") ?: findProperty("deploy-url") ?: "${rootProject.buildDir}/repo")
-            authentication {
-                val mavenUser = findProperty("kotlin.bintray.user") as String?
-                val mavenPass = findProperty("kotlin.bintray.password") as String?
-                if (url.scheme != "file" && mavenUser != null && mavenPass != null) {
-                    credentials {
-                        username = mavenUser
-                        password = mavenPass
-                    }
-                }
-            }
         }
     }
 }
