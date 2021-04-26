4dc65c6a3e91665caacd15a152272d99f458bb6b
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2021-04-26 17:17:25 +0300

    JVM_IR KT-45103 optimize direct invoke for lambdas and callable refs

--- compiler/testData/codegen/bytecodeText/constClosureOptimization.kt
+++ compiler/testData/codegen/bytecodeText/constClosureOptimization.kt
@@ -1,20 +1,23 @@
 fun test() {
 
     fun local(){
-        {
+        val lam = {
             //static instance access
             local()
-        }()
+        }
+        lam()
     }
 
     //static instance access
-    {
+    val lam = {
         //static instance access
         local()
-    }()
+    }
+    lam()
 
     //static instance access
-    (::local)()
+    val cr = ::local
+    cr()
 }
 
 // JVM_TEMPLATES
@@ -23,6 +26,6 @@ fun test() {
 // 1 GETSTATIC ConstClosureOptimizationKt\$test\$3\.INSTANCE
 
 // JVM_IR_TEMPLATES
-// 1 GETSTATIC ConstClosureOptimizationKt\$test\$1.INSTANCE
-// 1 GETSTATIC ConstClosureOptimizationKt\$test\$2.INSTANCE
-// 1 GETSTATIC ConstClosureOptimizationKt\$test\$local\$1.INSTANCE
+// 1 GETSTATIC ConstClosureOptimizationKt\$test\$cr\$1.INSTANCE
+// 1 GETSTATIC ConstClosureOptimizationKt\$test\$lam\$1.INSTANCE
+// 1 GETSTATIC ConstClosureOptimizationKt\$test\$local\$lam\$1.INSTANCE
