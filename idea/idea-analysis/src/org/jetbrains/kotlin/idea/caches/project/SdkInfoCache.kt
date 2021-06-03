aec2875cf32237738420acac6e0f99937a1fa087
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-06-03 17:04:16 +0200

    BFS of SdkInfo on moduleInfo and reuse cached libraryInfo dependencies
    Optimize SdkInfo lookup for a huge monorepo with no SdkInfo
    
    #KT-47065 Fixed
    Relates to #KT-46622

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/project/SdkInfoCache.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/project/SdkInfoCache.kt
@@ -6,12 +6,10 @@
 package org.jetbrains.kotlin.idea.caches.project
 
 import com.intellij.openapi.components.ServiceManager
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
-import com.intellij.util.containers.ConcurrentWeakKeySoftValueHashMap
-import com.intellij.util.containers.ContainerUtil
 import org.jetbrains.kotlin.analyzer.ModuleInfo
 import org.jetbrains.kotlin.caches.project.cacheInvalidatingOnRootModifications
+import java.util.ArrayDeque
 
 /**
  * Maintains and caches mapping ModuleInfo -> SdkInfo *form its dependencies*
@@ -47,6 +45,35 @@ class SdkInfoCacheImpl(private val project: Project) : SdkInfoCache {
         return cache[moduleInfo]
     }
 
-    private fun doFindSdk(moduleInfo: ModuleInfo): SdkInfo? =
-        moduleInfo.dependencies().lazyClosure { it.dependencies() }.firstOrNull { it is SdkInfo } as SdkInfo?
+    private fun doFindSdk(moduleInfo: ModuleInfo): SdkInfo? {
+        val libraryDependenciesCache = LibraryDependenciesCache.getInstance(this.project)
+
+        val checkedLibraryInfo = mutableSetOf<ModuleInfo>()
+        val stack = ArrayDeque<ModuleInfo>()
+
+        stack += moduleInfo
+
+        // bfs
+        while (stack.isNotEmpty()) {
+            val poll = stack.poll()
+            if (!checkedLibraryInfo.add(poll)) continue
+
+            val dependencies = run {
+                if (poll is LibraryInfo) {
+                    val (libraries, sdks) = libraryDependenciesCache.getLibrariesAndSdksUsedWith(poll)
+                    sdks.firstOrNull()?.let { return it }
+                    libraries
+                } else {
+                    poll.dependencies()
+                }
+            }.filter { !checkedLibraryInfo.contains(it) }
+
+            dependencies.firstOrNull { it is SdkInfo }
+                ?.let { return it as SdkInfo }
+
+            stack += dependencies
+        }
+
+        return null
+    }
 }
