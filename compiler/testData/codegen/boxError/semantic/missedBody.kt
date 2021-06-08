9800af86c84eca94a0698d2ac7af6e60d0aba88d
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2021-06-08 13:16:55 +0300

    Revert "~ update test data for code with errors"
    This reverts commit f51f45e4

--- compiler/testData/codegen/boxError/semantic/missedBody.kt
+++ compiler/testData/codegen/boxError/semantic/missedBody.kt
@@ -13,11 +13,7 @@ fun foo(): String {
 // FILE: b.kt
 
 fun box(): String {
-    try {
-        val r = foo()
-        if (r is String) return r
-    } catch (e: Throwable) {
-        return "OK"
-    }
-    return "FAIL 2"
+    val r = foo()
+    if (r is String) return r
+    return "OK"
 }
