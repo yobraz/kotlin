5eafe511abec905eef96719cb67b98f1082a3b19
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2021-06-08 17:18:24 +0300

    Fix quotes trimming

--- compiler/fir/modularized-tests/build.gradle.kts
+++ compiler/fir/modularized-tests/build.gradle.kts
@@ -63,7 +63,7 @@ projectTest {
         val argsExt = project.findProperty("fir.modularized.jvm.args") as? String
         if (argsExt != null) {
             val paramRegex = "([^\"]\\S*|\".+?\")\\s*".toRegex()
-            jvmArgs(paramRegex.findAll(argsExt).map { it.groupValues[1] }.toList())
+            jvmArgs(paramRegex.findAll(argsExt).map { it.groupValues[1].trim('"') }.toList())
         }
     }
     jvmArgs("-XX:ReservedCodeCacheSize=512m")
