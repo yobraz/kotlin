5b4c7cf67a2e7d21f9a58938334dbdddcaf93327
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2021-07-14 19:44:53 +0700

    disable mimalloc on iosArm32

--- kotlin-native/shared/src/main/kotlin/org/jetbrains/kotlin/konan/target/KonanTargetExtenstions.kt
+++ kotlin-native/shared/src/main/kotlin/org/jetbrains/kotlin/konan/target/KonanTargetExtenstions.kt
@@ -21,7 +21,7 @@ fun KonanTarget.supportsMimallocAllocator(): Boolean =
         is KonanTarget.LINUX_ARM32_HFP -> true
         is KonanTarget.ANDROID_X64 -> true
         is KonanTarget.ANDROID_ARM64 -> true
-        is KonanTarget.IOS_ARM32 -> true
+        is KonanTarget.IOS_ARM32 -> false // TODO: clang msg: misaligned atomic operation may incur significant performance penalty
         is KonanTarget.IOS_ARM64 -> true
         is KonanTarget.IOS_X64 -> true
         else -> false // watchOS/tvOS/android_x86/android_arm32 aren't tested; linux_mips32/linux_mipsel32 need linking with libatomic.
