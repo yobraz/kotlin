faab75d1c6b32afa5b913c1d56b95923f528313e
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-08-04 14:57:49 +0300

    ++ test

--- compiler/fir/analysis-tests/testData/resolveWithStdlib/intellij/hideBySeverity.kt
+++ compiler/fir/analysis-tests/testData/resolveWithStdlib/intellij/hideBySeverity.kt
@@ -1,32 +1,24 @@
-import java.util.Collections
-import java.util.concurrent.ConcurrentHashMap
 import kotlin.reflect.KProperty
 import kotlin.properties.ReadWriteProperty
 
-interface StoredProperty<T> {
-    var name: String?
-
-    fun getValue(thisRef: BaseState): T
-    fun setValue(thisRef: BaseState, value: T)
-}
-
 // type must be exposed otherwise `provideDelegate` doesn't work
-abstract class StoredPropertyBase<T> : StoredProperty<T>, ReadWriteProperty<BaseState, T> {
-    override var name: String? = null
+abstract class StoredPropertyBase<T> : ReadWriteProperty<BaseState, T>
 
-    operator fun provideDelegate(thisRef: Any, property: KProperty<*>): ReadWriteProperty<BaseState, T> {
-        name = property.name
+abstract class StoredPropertyDerived<T> : StoredPropertyBase<T>() {
+    operator fun provideDelegate(thisRef: Any, property: KProperty<*>): StoredPropertyBase<T> {
         return this
     }
-
-    override operator fun getValue(thisRef: BaseState, property: KProperty<*>): T = getValue(thisRef)
-    override operator fun setValue(thisRef: BaseState, property: KProperty<*>, value: T) = setValue(thisRef, value)
 }
 
+
 abstract class BaseState {
-    protected abstract fun <PROPERTY_TYPE> property(initialValue: PROPERTY_TYPE, isDefault: (value: PROPERTY_TYPE) -> Boolean): StoredPropertyBase<PROPERTY_TYPE>
+    protected abstract fun <PROPERTY_TYPE> propertyDerived(initialValue: PROPERTY_TYPE): StoredPropertyDerived<PROPERTY_TYPE>
+
+    protected abstract fun <PROPERTY_TYPE> propertyBase(initialValue: PROPERTY_TYPE): StoredPropertyBase<PROPERTY_TYPE>
 }
 
 abstract class Some : BaseState() {
-    val hideBySeverity: MutableSet<Int> by <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>property<!>(Collections.newSetFromMap(ConcurrentHashMap()), { it.<!OVERLOAD_RESOLUTION_AMBIGUITY!>isEmpty<!>() })
+    val hideBySeverityDerived: MutableSet<Int> by <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>propertyDerived<!>(mutableSetOf())
+
+    val hideBySeverityBase: MutableSet<Int> by propertyBase(mutableSetOf())
 }
