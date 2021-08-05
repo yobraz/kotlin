4c49a0eba7d8eec6a1c092d34423be0d909560a2
ebukreev <bukreevdev@gmail.com>
2021-08-05 15:42:20 +0300

    Union Types: fix VerifyError

--- compiler/fir/fir2ir/testData/codegen/box/unionTypes/callFromCatch.kt
+++ compiler/fir/fir2ir/testData/codegen/box/unionTypes/callFromCatch.kt
@@ -6,8 +6,7 @@ fun box(): String {
     try {
         throw NullPointerException()
     } catch (e: IllegalArgumentException | NullPointerException) {
-        // TODO: Stackmap table contain java/lang/Object type instead of RuntimeException
-//        return foo(e)
+        return foo(e)
     }
-    return "OK"
+    return "Fail"
 }
