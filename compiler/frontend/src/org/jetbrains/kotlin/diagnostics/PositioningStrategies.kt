f945d52e09e9e046b34c1f73af0d8934f456d212
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-08-09 13:10:42 +0300

    Drop support for T!! syntactic structure
    ^KT-26245 In Progress

--- compiler/frontend/src/org/jetbrains/kotlin/diagnostics/PositioningStrategies.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/diagnostics/PositioningStrategies.kt
@@ -835,7 +835,6 @@ object PositioningStrategies {
         return when (this) {
             is KtUserType -> referenceExpression
             is KtNullableType -> innerType?.getReferencedTypeExpression()
-            is KtDefinitelyNotNullType -> innerType?.getReferencedTypeExpression()
             else -> null
         }
     }
