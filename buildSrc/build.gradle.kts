a2d958cb2fd016898e28aeddfc5e0d289fee6652
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2021-08-11 22:23:12 +0300

    *WIP* Disable warnings as errors for buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -195,7 +195,7 @@ java {
 }
 
 tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions.allWarningsAsErrors = true
+    // TODO: kotlinOptions.allWarningsAsErrors = true
     kotlinOptions.freeCompilerArgs += listOf(
         "-Xopt-in=kotlin.RequiresOptIn",
         "-Xskip-runtime-version-check",
